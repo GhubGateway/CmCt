86cb8d4f6b910752e4039354b76f779d9c72dff2
Abigail Harris <avharris@sas.upenn.edu>
2024-07-19 14:11:07 +0000

    Resolving merge conflict due to accidental git stash pop

--- notebooks/Gravimetry/mascons.py
+++ notebooks/Gravimetry/mascons.py
@@ -76,10 +76,11 @@ def load_gsfc_solution(h5_filename, lon_wrap='pm180'):
 
 def points_to_mascons(mascons, I_, lats, lons, values):
     """
-    For each mascon i such that I_[i] == true, this function will set mscn_mean[j] to be the 
-    average of all values[z] such that lats[z] and lons[z] is contained within the bounds of 
-    mascon[i] as defined by mascons.lat_center[i], mascons.lon_center[i], mascons.lat_span[i], and
-    mascons.lon_span[i]. 
+    For each mascon i such that I_[i] == true, this function will set (mscn_mean[k])[j] to be the 
+    average of all (values[k])[z] such that lats[z] and lons[z] is contained within the bounds of 
+    mascon i as defined by mascons.lat_center[i], mascons.lon_center[i], mascons.lat_span[i], and
+    mascons.lon_span[i]. If there are no values z such that this is true, (mscn_mean[k])[j] will 
+    be np.nan
     Note that j does not have to be equal to i, and in fact it often is not. mscn_mean[0] will 
     correspond to the first entry of I_ which is true, mscn_mean[1] will correspond to the second 
     entry of I_ which is true, and so on.
@@ -87,13 +88,13 @@ def points_to_mascons(mascons, I_, lats, lons, values):
     Parameters:
     mascons: A mascons object
     I_ : A boolean array with shape (mascons.N_mascons,)
-    lats, lons, values: All three are 1D arrays which must all have the same length. value[z] 
-    corresponds to the point at lats[z], lons[z]. The function np.nanmean must be able to 
-    operate on the entries of the values array
+    lats, lons: Both are 1D arrays of floats which must all have the same length
+    values: A list of 1D arrays of floats, each of which must have the same length as lats and lons. 
+    For any v = value[i], v[z] corresponds to the point at lats[z], lons[z]
 
     Returns:
-    mscn_mean: A 1D array of length np.sum(I_) and whose entries are the same datatype as the 
-    entries of the values array
+    mscn_mean: A list of 1D array of floats of length np.sum(I_). mscn_mean[k] corresponds to 
+    values[k]. 
     """
     N_GIS_mascons = np.sum(I_)   # number of mascons that are on the greenland ice sheet
     d2r = np.pi/180
@@ -102,12 +103,14 @@ def points_to_mascons(mascons, I_, lats, lons, values):
     max_lats = mascons.lat_centers + mascons.lat_spans/2
     min_lons = mascons.lon_centers - mascons.lon_spans/2
     max_lons = mascons.lon_centers + mascons.lon_spans/2
-    
-    mscn_mean = np.nan * np.ones(N_GIS_mascons)
+
+    # Initialize output to all NaNs
+    mscn_mean = []
+    for k in range(len(values)):
+        mscn_mean.append(np.nan * np.ones(N_GIS_mascons))  
 
     indices = np.array(range(mascons.N_mascons))[I_]
-    j = 0
-    for i in indices:
+    for i, j in enumerate(indices):
     
         if np.min(lats) > max_lats[i]:
             continue
@@ -119,28 +122,51 @@ def points_to_mascons(mascons, I_, lats, lons, values):
             continue
         
         K_ = (lats >= min_lats[i]) & (lats < max_lats[i]) & (lons >= min_lons[i]) & (lons < max_lons[i])
-        m = values[K_]
-        m_lats = lats[K_]
-        
-        m_lats = m_lats[~np.isnan(m)]
-        m = m[~np.isnan(m)]
-        
-        if len(m) == 0:
-            continue
-        if np.sum(~np.isnan(m)) == 0:
-            continue
 
-        cos_weight = np.cos(m_lats*d2r)
-        mscn_mean[j] = np.nanmean(m) # * cos_weight) / (np.sum(cos_weight) * len(m))
+        for k in range(len(values)):
+            m = (values[k])[K_]
+            m_lats = lats[K_]
+            
+            m = m[~np.isnan(m)]
+            m_lats = m_lats[~np.isnan(m)]
         
-        j = j + 1
+            if len(m) == 0:
+                continue
+            if np.sum(~np.isnan(m)) == 0:
+                continue
+            (mscn_mean[k])[j] = np.nanmean(m)
     return mscn_mean
 
 def YYYY_MM_DD_to_days_since_Jan_0_2002(date_str):
     date_obj = datetime.datetime.strptime(date_str, '%Y-%m-%d')
     return cftime.date2num(date_obj, "days since 2001-12-31")
 
-def calc_mascon_delta_cmwe(mascon, start_date, end_date, I_):
+def calc_many_mascon_delta_cmwes(mascon, times_needed, I_):
+    """
+    Parameters:
+    mascon: A mascons object
+    times_needed: An iterable of tuples of the form ('YYYY-MM-DD', 'YYYY-MM-DD') where the first entry 
+    represents a start date and the second entry represents an end date. Let n be the number of 
+    tuples in times_needed
+    I_: A 1D boolean array with the same size as the first axis of mascon.cmwe
+
+    Returns: 
+    A list of 1D arrays each with shape (np.sum(I_)). Entry i corresponds to the start and end date 
+    given in the (i+1)th tuple given by times_needed. See calc_mascon_delta_cmwe for a description of the 
+    meaning of each entry of the list
+    """
+    cmwe_I_ = mascon.cmwe[I_]   # Only include those mascons indicated in I_
+    interp_obj = interp1d(mascon.days_middle, cmwe_I_, axis = 1, assume_sorted = True, bounds_error = False, fill_value = "extrapolate")
+
+    output = []
+    for start_date, end_date in times_needed:
+        cmwe = calc_mascon_delta_cmwe(mascon, start_date, end_date, I_, interp_obj)
+        if cmwe is None:
+            return None
+        output.append(cmwe)
+    return output
+
+def calc_mascon_delta_cmwe(mascon, start_date, end_date, I_, interp_obj = None):
     """
     Given a mascons object, strings indicating start and end dates, and a boolean array, this function
     returns the change in the cmwe variable for the mascons object between the start and end dates.
@@ -155,6 +181,7 @@ def calc_mascon_delta_cmwe(mascon, start_date, end_date, I_):
     end_date: A string of the form "YYYY-MM-DD". An error will be printed and the function will return 
     None if end_date occurs after mascon.days_end[-1]
     I_: A 1D boolean array with the same size as the first axis of mascon.cmwe
+    interp_obj: Used by the calc_many_mascon_delta_cmwes function
 
     Returns:
     A 1D array with shape (np.sum(I_)). This represents (for the rows of mascon.cmwe indicated by I_, 
@@ -175,10 +202,12 @@ def calc_mascon_delta_cmwe(mascon, start_date, end_date, I_):
     cmwe_I_ = mascon.cmwe[I_]   # Only include those mascons indicated in I_
     
     # Create an interpolation object to estimate the ice thickness on the start date and end date
-    interp_obj = interp1d(mascon.days_middle, cmwe_I_, axis = 1, assume_sorted = True, bounds_error = False, fill_value = "extrapolate")
+    if interp_obj is None:
+        interp_obj = interp1d(mascon.days_middle, cmwe_I_, axis = 1, assume_sorted = True, bounds_error = False, fill_value = "extrapolate")
     
     # Note that the only case in which interp1d will need to extrapolate is when days_start[0] <= t_0 < days_middle[0] or 
     # days_middle[-1] < t_1 < days_end[-1], in which case the extrapolation should not introduce too much error
+
     start_cmwe = interp_obj(t_0)
     end_cmwe = interp_obj(t_1)
     return np.array(end_cmwe - start_cmwe)
