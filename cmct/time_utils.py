32f1fd1b1fbb4623155eb26fed99b3eb9c1103ab
kushakum <kushakum@buffalo.edu>
2025-04-03 18:21:46 +0000

    Added try except block in imbie_comparison file

--- cmct/time_utils.py
+++ cmct/time_utils.py
@@ -14,8 +14,8 @@ def check_datarange(time_var, start_date_cftime, end_date_cftime):
     if min_time <= start_date_cftime <= max_time and min_time <= end_date_cftime <= max_time:
         print(f"The selected dates {start_date_cftime} and {end_date_cftime} are within the range of the model data.")
     else:
-        #raise ValueError(f"Error: The selected dates {start_date_cftime} or {end_date_cftime} are out of range. Model data time range is from {min_time} to {max_time}.")
-        print(f"The selected dates {start_date_cftime} and {end_date_cftime} are not within the range of the model data. Please select dates within the range.")
+        raise ValueError(f"The selected dates {start_date_cftime} or {end_date_cftime} are out of range. Model data time range is from {min_time} to {max_time}.")
+
 
 
 def days_in_year(date):
